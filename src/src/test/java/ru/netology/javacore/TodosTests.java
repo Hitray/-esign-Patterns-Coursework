f909af5851d3ff4df6f3c6b00127701b5b99583a
Hitray <75577433+Hitray@users.noreply.github.com>
2022-11-03 20:07:09 +0500

    Update TodosTests.java

--- src/src/test/java/ru/netology/javacore/TodosTests.java
+++ src/src/test/java/ru/netology/javacore/TodosTests.java
@@ -1,65 +1,62 @@
 package ru.netology.javacore;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.stream.Stream;
 
 public class TodosTests {
+    private static Todos todos;
 
-    @DisplayName("Проверка добавления новых задач")
-    @Test
-    public void testAddTask() {
-        Todos todos = new Todos();
-        todos.addTask("Первая");
-        todos.addTask("Вторая");
-        todos.addTask("Третья");
-        todos.addTask("Первая");
-        todos.addTask("Четвертая");
-        todos.addTask("Пятая");
-        todos.addTask("Шестая");
-        todos.addTask("Седьмая");
-        todos.addTask("Восьмая");
-        Set<String> actualResult = todos.getTasks();
-        Set<String> expectResult = new TreeSet<>(Arrays.asList("Первая", "Вторая", "Третья", "Четвертая", "Пятая", "Шестая", "Седьмая"));
-        Assertions.assertEquals(expectResult, actualResult);
+    @BeforeAll
+    public static void initTodos() {
+        todos = new Todos();
     }
 
-    @DisplayName("Проверка удаления задач")
-    @Test
-    public void testRemoveTask() {
-        Todos todos = new Todos();
-        todos.addTask("Первая");
-        todos.addTask("Вторая");
-        todos.addTask("Третья");
-        todos.removeTask("Вторая");
-        todos.removeTask("Первая");
-        todos.removeTask("Первая");
-
-        Set<String> actualResult = todos.getTasks();
-        Set<String> expectResult = new TreeSet<>(Arrays.asList("Третья"));
-        Assertions.assertEquals(expectResult, actualResult);
+    public static Stream<Arguments> methodSourceForAdd() {
+        return Stream.of(
+                Arguments.of("task1", true), //новые задачи должны добавляться
+                Arguments.of("task2", true),
+                Arguments.of("task1", false) //повторно задачу добавить нельзя
+        );
     }
 
-    @DisplayName("Проверка вывода всех задач")
-    @Test
-    public void testGetAllTasks() {
-        Todos todos = new Todos();
-        todos.addTask("Первая");
-        todos.addTask("Вторая");
-        todos.addTask("Третья");
-        todos.addTask("Первая");
-        todos.addTask("Четвертая");
-        todos.addTask("Пятая");
-        todos.addTask("Шестая");
-        todos.addTask("Седьмая");
-        todos.addTask("Восьмая");
-        String expectResult = "Вторая Первая Пятая Седьмая Третья Четвертая Шестая";
-        String actualResult = todos.getAllTasks();
-        Assertions.assertEquals(expectResult, actualResult);
+    public static Stream<Arguments> methodSourceForRemove() {
+        return Stream.of(
+                Arguments.of("task3", true),
+                Arguments.of("task4", false) 
+        );
     }
 
+    @ParameterizedTest
+    @MethodSource("methodSourceForAdd")
+    public void testAddTask(String task, boolean expectedResult) {
+        boolean actualResult = todos.addTask(task);
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @ParameterizedTest
+    @MethodSource("methodSourceForRemove")
+    public void testRemoveTask(String task, boolean expectedResult) {
+        todos.addTask("task3");
+        boolean actualResult = todos.removeTask(task);
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+
+    
+    @Test
+    void testGetAllTasks() {
+        Todos todosAll = new Todos();
+        todosAll.addTask("B");
+        todosAll.addTask("A");
+
+        String expectedResult = "[A, B]";
+        String actualResult = todosAll.getAllTasks();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
 }
