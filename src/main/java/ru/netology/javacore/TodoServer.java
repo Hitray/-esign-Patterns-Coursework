0176ca2da1a5a443d1a4202880b8ce6e4f12d812
Hitray <75577433+Hitray@users.noreply.github.com>
2022-11-03 19:58:22 +0500

    Update TodoServer.java

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -1,68 +1,78 @@
 package ru.netology.javacore;
 
-import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TodoServer {
-    private Todos todos;
     private int port;
+    private Todos todos;
+
+    private List<String> history = new ArrayList<>();
 
     public TodoServer(int port, Todos todos) {
         this.port = port;
         this.todos = todos;
-
     }
 
     public void start() throws IOException {
         System.out.println("Starting server at " + port + "...");
-        try (ServerSocket serverSocket = new ServerSocket(8989);) {
-            while (true) { 
-                try (
-                        Socket socket = serverSocket.accept();
-                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                ) {
-                   
-                    String input = in.readLine();
-                    JsonObject jsonObject = new Gson().fromJson(input, JsonObject.class).getAsJsonObject();
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
 
-                    String operation = jsonObject.get("type").getAsString();
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader reader = new BufferedReader
+                             (new InputStreamReader(clientSocket.getInputStream()))) {
+
+                    System.out.println("New connection accepted");
+
+                    JsonElement element = JsonParser.parseString(reader.readLine());
+                    JsonObject jsonObject = element.getAsJsonObject();
+                    String type = jsonObject.get("type").getAsString();
+                    if (type.equals("RESTORE")) {
+                        undoOperation();
+                        writer.println(todos.getAllTasks());
+                        continue;
+                    }
                     String task = jsonObject.get("task").getAsString();
-                    String result = applyOperationToTask(operation, task);
-
-                    out.println(result + ": " + todos.getAllTasks());
+                    switch (type) {
+                        case "ADD":
+                            todos.addTask(task);
+                            history.add(type + " " + task);
+                            break;
+                        case "REMOVE":
+                            todos.removeTask(task);
+                            history.add(type + " " + task);
+                    }
+                    writer.println(todos.getAllTasks());
                 }
             }
-        } catch (IOException e) {
-            System.out.println("Не могу стартовать сервер");
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 
-    public String applyOperationToTask(String operation, String task) {
-        String result;
-        if (operation.equals("ADD")) {
-            if (todos.addTask(task)) {
-                result = "Задача \"" + task + "\" успешно добавлена в список";
-            } else {
-                result = "Задача \"" + task + "\" была добавлена в список ранее";
+    public void undoOperation() {
+        if (!history.isEmpty()) {
+            String[] lastOperation = history.get(history.size() - 1).split(" ");
+            String type = lastOperation[0];
+            String task = lastOperation[1];
+            switch (type) {
+                case "ADD":
+                    todos.removeTask(task);
+                    break;
+                case "REMOVE":
+                    todos.addTask(task);
             }
-        } else if (operation.equals("REMOVE")) {
-            if (todos.removeTask(task)) {
-                result = "Задача \"" + task + "\" успешно удалена из списка";
-            } else {
-                result = "Задачи \"" + task + "\" нет в списке";
-            }
-        } else {
-            result = "unknown command";
+            history.remove(history.size() - 1);
         }
-        return result;
     }
 }
